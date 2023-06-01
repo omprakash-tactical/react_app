366e3bc980f3acf55162410ac48779203dd04426
Om Prakash <om.prakash@sw-at.com>
2023-06-01 16:15:29 +0530

    third commit

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -5,41 +5,41 @@ function Header() {
   const navigate = useNavigate();
   return (
     <>
-     <div className="p-1 mb-1 bg-info text-white">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+     <div className="mb-1 text-white header-section">
+      <nav className="navbar navbar-expand-lg header_shadow">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/" alt="">
-            OM
+          <a className="navbar-brand nav_color" href="/school" alt="">
+            East & West
           </a>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <li className="nav-link" onClick={() => navigate("/")}>
                   Home
                 </li>
-              </li>
+              </li> */}
 
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <li className="nav-link" onClick={() => navigate("/about")}>
                   About
                 </li>
-              </li>
-              <li className="nav-item">
+              </li> */}
+              {/* <li className="nav-item">
                 <li className="nav-link" onClick={() => navigate("/school")}>
                   School-app
                 </li>
-              </li>
+              </li> */}
               <li className="nav-item">
                 <a
                   href="https://www.eastandwesthighschool.org/home"
                   alt=""
                   target="_blank"
                 >
-                  <li className="nav-link">School</li>
+                  <li className="nav-link nav_color">Old School web</li>
                 </a>
               </li>
-              <li class="nav-item dropdown">
+              {/* <li class="nav-item dropdown">
                 <a
                   class="nav-link dropdown-toggle"
                   href="#"
@@ -75,7 +75,7 @@ function Header() {
                     </a>
                   </li>
                 </ul>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
